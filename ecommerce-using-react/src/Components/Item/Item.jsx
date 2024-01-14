0cc832aeaa4e2dd8c15ed8e40f2b5afc2b144c17
Akash P S <116643229+ItzmeAkash@users.noreply.github.com>
2024-01-14 22:47:03 +0530

    offers and popular added

--- ecommerce-using-react/src/Components/Item/Item.jsx
+++ ecommerce-using-react/src/Components/Item/Item.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Item.css'
+
 const item = (props) => {
   return (
     <div className='item'>
--- ecommerce-using-react/src/Components/Offers/Offers.jsx
+++ ecommerce-using-react/src/Components/Offers/Offers.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import './Offers.css'
+import exclucive_image from '../Assets/exclusive_image.png'
 const offers = () => {
   return (
     <div className='offers'>
          <div className="offers-left">
            <h1>Exclusive</h1>
            <h1>Offers Foor You</h1>
-           p
+           <p>ONLY ON BEST SELLERS PRODUCTS</p>
+           <button>Check now</button>
          </div>
          <div className="offers-right">
-
+            <img src={exclucive_image} alt="" />
          </div>
     </div>
   )
