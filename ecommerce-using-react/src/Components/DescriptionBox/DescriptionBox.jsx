083fbd0e847deb917a53b77a97e063934c961869
Akash P S <116643229+ItzmeAkash@users.noreply.github.com>
2024-01-17 15:33:15 +0530

    review box added

--- ecommerce-using-react/src/Components/DescriptionBox/DescriptionBox.jsx
+++ ecommerce-using-react/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -3,7 +3,18 @@ import './DescriptionBox.css'
 const DescriptionBox = () => {
   return (
     <div className='descriptionbox'>
-      
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+                
+            
+        </div>
+        <div className="descriptionbox-description">
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum adipisci delectus molestiae non odit voluptas, praesentium corrupti voluptatum vitae assumenda qui sapiente illum eaque dolor et dicta provident nesciunt sunt!</p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis rem, blanditiis ad sed sapiente provident voluptates exercitationem, maxime eos error, porro ipsam ducimus eveniet? Aliquid itaque sunt est cumque nulla.
+        </p>
+        </div>
     </div>
   )
 }
